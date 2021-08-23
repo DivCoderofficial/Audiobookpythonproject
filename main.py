cfd54fba229f3b4e2009980975955e687b7449c3
DivCoderofficial <87817638+DivCoderofficial@users.noreply.github.com>
2021-08-23 16:51:04 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import pyttsx3
+import PyPDF2
+book = open("Learning_Python.pdf", "rb")
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+print(pages)
+speaker = pyttsx3.init()
+for num in range(8, pages):
+    page = pdfReader.getPage(8)
+    text = page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()
